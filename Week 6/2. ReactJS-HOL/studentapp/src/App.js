bdac090affb9fdd00d966c96ad6f4cbd63b677c2
Govardhan Reddy <119473283+govardhan6302310410@users.noreply.github.com>
2025-07-29 09:03:04 +0530

    Add files via upload

---
+++ Week 6/2. ReactJS-HOL/studentapp/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+function App() {
+  return (
+
+  <div className="container">
+    <h1>Student Management Portal</h1>
+    <Home />
+    <About />
+    <Contact />
+  </div>
+
+  );
+}
+
+export default App;
